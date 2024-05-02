5a7f06d066799bc3efa8b3f6e942d145607f9bc4
mitsu055 <uxtsuu.mitsu@icloud.com>
2024-05-02 22:06:32 +0900

    成り駒の動き追加

--- set_koma.js
+++ set_koma.js
@@ -196,14 +196,30 @@ window.onload = function() {
             case 'r':
                 // 飛車の場合、直線上に動ける
                 return fromRow === toRow || fromCol === toCol;
+            case 'Nr': //龍王
+            case 'NR':
+                return (Math.abs(toRow - fromRow) <= 1 && Math.abs(toCol - fromCol) <= 1) ||
+           (fromRow === toRow || fromCol === toCol);
             case 'B': // 角行
             case 'b':
                 // 角行の場合、斜めに動ける
                 return Math.abs(toRow - fromRow) === Math.abs(toCol - fromCol);
+            case 'NB': //龍馬
+            case 'Nb':
+                return (Math.abs(toRow - fromRow) <= 1 && Math.abs(toCol - fromCol) <= 1) ||
+                (Math.abs(toRow - fromRow) === Math.abs(toCol - fromCol));
             case 'G': // 金将
+            case 'NP':
+            case 'NL':
+            case 'NN':
+            case 'NS':
                 return Math.abs(toRow - fromRow) <= 1 && Math.abs(toCol - fromCol) <= 1 &&
                 !(toRow - fromRow === 1 && toCol !== fromCol);
             case 'g': // 金将
+            case 'Np':
+            case 'Nl':
+            case 'Nn':
+            case 'Ns':
                 return Math.abs(toRow - fromRow) <= 1 && Math.abs(toCol - fromCol) <= 1 &&
                 !(toRow - fromRow === -1 && toCol !== fromCol);
             case 'S': // 銀将
@@ -224,9 +240,6 @@ window.onload = function() {
                 return (fromRow-toRow) == 1 && fromCol === toCol;
             case 'p':
                 return (fromRow-toRow)== -1 && fromCol === toCol;
-            case 'Np':
-                return Math.abs(toRow - fromRow) <= 1 && Math.abs(toCol - fromCol) <= 1 &&
-                !(toRow - fromRow === -1 && toCol !== fromCol);
             default:
                 return false; // その他の駒は無効な移動
         }
